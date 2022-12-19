e62ee73cfd365f321ea73389721ef2ade291c14e
ilyalazarenkoit <ilyalazarenkoit@gmail.com>
2022-12-19 16:18:16 +0200

    first commit before check

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,15 +1,14 @@
-import { Component } from 'react';
+
 import { TfiFaceSad } from 'react-icons/tfi';
 import LoaderStyles from '../Loader/Loader.module.css';
 
-class Error extends Component {
-  render() {
+const Error = () => {
     return (
       <h2 className={LoaderStyles.loader}>
         No images found for your request <TfiFaceSad />
       </h2>
     );
   }
-}
+
 
 export { Error };
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,28 +1,23 @@
-import { Component } from 'react';
 import LoaderStyles from '../Loader/Loader.module.css';
 import { MutatingDots } from 'react-loader-spinner';
 
-class Loader extends Component {
-  render() {
-    return (
-      <div className={LoaderStyles.loader}>
-        <h2>
-          Loading...
-          <MutatingDots
-            height="100"
-            width="100"
-            color="rgb(93, 93, 237)"
-            secondaryColor="rgb(93, 93, 237)"
-            radius="12.5"
-            ariaLabel="mutating-dots-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
-          />
-        </h2>
-      </div>
-    );
-  }
-}
-
-export { Loader };
+export const Loader = () => {
+  return (
+    <div className={LoaderStyles.loader}>
+      <h2>
+        Loading...
+        <MutatingDots
+          height="100"
+          width="100"
+          color="rgb(93, 93, 237)"
+          secondaryColor="rgb(93, 93, 237)"
+          radius="12.5"
+          ariaLabel="mutating-dots-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+      </h2>
+    </div>
+  );
+};
